181fd15d3a66945cd3b0cf2b0262550fb1b7104b
ctfische <90360025+ctfische@users.noreply.github.com>
2021-09-08 20:12:03 -0400

    Add files via upload

---
+++ Week 2 recitation.py
@@ -0,0 +1,14 @@
+'''
+points_obtained = 98
+total_points = 100
+percentage = (points_obtained/total_points)*100
+print(int(percentage))
+'''
+x = "I'm a UB student"
+print(x[0])
+print(x[5])
+print(x[10])
+print(x[-1])
+
+
+
